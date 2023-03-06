b5a6f712bbb581740160ad15d2400efdadba0077
Vitória Sesana <vitoriasesana11@gmail.com>
2023-03-06 13:46:09 -0300

    Upload Dimensões 1 e 3

---
+++ Script/IDF/IDF_calculo.R
@@ -0,0 +1,63 @@
+# função que irá chamar as 6 dimensões e calculará o idf
+
+IDF <- function(base, 
+                salario_minimo) {
+  
+  require(data.table)
+  
+  # chamando as funções das 6 dimensões -------------------------------------
+  
+  source("Script/DIM01/DIM01.R")
+  # source("Script/DIM02/DIM02.R")
+  source("Script/DIM03/DIM03.R")
+  # source("Script/DIM04/DIM04.R")
+  # source("Script/DIM05/DIM05.R")
+  # source("Script/DIM06/DIM06.R")
+  
+
+
+  # calculando cada dimensão ------------------------------------------------
+
+  dimensao1 <- D1(base) 
+  # dimensao2 <- D2(base) 
+  dimensao3 <- D3(base, salario_minimo)
+  # dimensao4 <- D4(base) 
+  # dimensao5 <- D5(base) 
+  # dimensao6 <- D6(base) 
+  
+  lista_dimensoes <- list(dimensao1,
+                          # dimensao2,
+                          dimensao3
+                          # dimensao4,
+                          # dimensao5,
+                          # dimensao6
+                          )
+  
+  # concatenando as 6 dimensões ---------------------------------------------
+
+  source("Script/AUXILIARES/merge_dados.R",
+         encoding = "UTF-8")
+  
+  dado <- merge_dados(dados = lista_dimensoes,
+                      by = "d.cod_familiar_fam",
+                      sort = TRUE)
+
+  # calculando o IDF --------------------------------------------------------
+  
+  dado[, idf := rowMeans(as.data.table(.(d1,
+                                         # d2,
+                                         d3
+                                         # d4,
+                                         # d5,
+                                         # d6
+                                         )))]
+
+  # saída -------------------------------------------------------------------
+  
+  saida <- dado[, .(d.cod_familiar_fam, 
+                    idf)]
+  
+  return(saida)
+}
+
+
