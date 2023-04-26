2a0aade07d5e1456a3e7ee287e13d11351652e82
Victor Nunes Toscano <victor.ijsn@gmail.com>
2023-04-26 16:51:39 -0300

    Dimensão 05 - Componente 01
    ## NAGI - SETADES / 2023
    ## Responsável: Victor Toscano
    
    # Cálculo do Índice de Desenvolvimento Familiar
    # Dimensão: 5. Desenvolvimento Infantil
    # Componente: 5.1. Trabalho precoce

---
+++ Script/DIM05/D5_C1_indicador2.R
@@ -0,0 +1,70 @@
+## NAGI - SETADES / 2023
+## Responsável: Victor Toscano
+
+# Cálculo do Índice de Desenvolvimento Familiar
+# Dimensão: 5. Desenvolvimento Infantil
+# Componente: 5.1. Trabalho precoce
+# Indicador: 5.1.2. Ausência de criancas menores de 14 anos trabalhando
+
+D5_C1_I2 <- function(base){
+  
+  require(data.table)
+  
+  # chamando as colunas necessárias -----------------------------------------
+  
+  if ("aux_idade" %in% colnames(base) & "aux_pessoa_ocupada" %in% colnames(base)) {
+    
+    dado <- base[, .(d.cod_familiar_fam,
+                     p.ind_trabalho_infantil_pessoa,
+                     aux_idade,
+                     aux_pessoa_ocupada)]
+    
+  } else {
+    
+    if (!("aux_idade" %in% colnames(base))) {
+      
+      source("Script/AUXILIARES/auxiliar_idade.R") 
+      
+      dado <- auxiliar_idade(base)
+      
+    }
+    
+    if (!("aux_pessoa_ocupada" %in% colnames(base))) {
+      
+      source("Script/AUXILIARES/auxiliar_pessoa_ocupada.R") 
+      
+      dado <- auxiliar_pessoa_ocupada(base)
+      
+    }
+    
+    dado <- base[, .(d.cod_familiar_fam,
+                     p.ind_trabalho_infantil_pessoa,
+                     aux_idade,
+                     aux_pessoa_ocupada)]
+  }
+  
+  # Criancas menores de 14 anos trabalhando ---------------------------------
+  
+  dado[aux_idade<14L, marca_14anos_trab:=fifelse(aux_pessoa_ocupada==1L |
+                                                   p.ind_trabalho_infantil_pessoa==1L,
+                                                 1L, 0L)]
+  
+  # calculando o indicador --------------------------------------------------
+  
+  dado <- dado[, .(total_14anos_trab = 
+                     sum(marca_14anos_trab, na.rm = TRUE)),
+               by = c("d.cod_familiar_fam")]
+  
+  dado[, d5_c1_i2 := fifelse(total_14anos_trab > 0, 0, 1)]
+  
+  
+  # Validação ---------------------------------------------------------------
+  
+  
+  # saída -------------------------------------------------------------------
+  
+  saida <- dado[, .(d.cod_familiar_fam,
+                    d5_c1_i2)]
+  
+  return(saida)
+}
